9a8c3c24c91ddf0ef308d7404750021a153d2d94
(Student  C00296706) Riona Kelly <C00296706@setu.ie>
2023-11-10 12:52:02 +0000

    w added

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -23,7 +23,7 @@ int main()
 		return 0;
 	}
 
-	std::cout << "Pick a letter from A to C or Y to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to C or W to Z" << std::endl; // change this line each iteration
 
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
@@ -101,6 +101,29 @@ int main()
 		if (region == 5)
 		{
 			std::cout << "Cocos (Keeling) Islands , Cook Islands" << std::endl;
+		}
+	}
+	if (letter == 'w' || letter == 'W') // written by Riona
+	{
+		if (region == 1)
+		{
+			std::cout << "There are no Ws in the Americas" << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "There are no Ws in Europe" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "Western Sahara" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are no Ws in Asia" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "Wallis and Futuna Islands" << std::endl;
 		}
 	}
 	if (letter == 'x' || letter == 'X') // written by Riona
